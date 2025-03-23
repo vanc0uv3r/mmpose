2d00072406dd0130dac467f20a7df41fbbce3b56
yiwangio <113520262+yiwangio@users.noreply.github.com>
2025-03-23 11:29:11 +0300

    added custom dataset

---
+++ mmpose/datasets/datasets/body/vertebrae.py
@@ -0,0 +1,8 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+from mmpose.registry import DATASETS
+from ..base import BaseCocoStyleDataset
+
+
+@DATASETS.register_module(name="VertebraeDataset")
+class VertebraeDataset(BaseCocoStyleDataset):
+    METAINFO: dict = dict(from_file='configs/_base_/datasets/vertebrae.py')
